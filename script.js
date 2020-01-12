daeaa9d0e78b73f27b867ecd95260aab05c7478b
Kelvinator07 <kelvinator4leo@gmail.com>
2020-01-12 20:03:38 +0100

    ES6 Arrow Functions Lexical this keyword

--- script.js
+++ script.js
@@ -122,3 +122,85 @@ ages6 = years.map((current, index, array) => {
 });
 
 console.log(ages6);
+
+
+
+// Arrow Functions Lexical 'this'
+
+// ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function() {
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+
+}
+
+box5.clickMe(); 
+
+// ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', () => {
+            const str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+
+}
+
+box6.clickMe(); 
+
+// ES6
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+        const self = this;
+        document.querySelector('.green').addEventListener('click', () => {
+            const str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+
+}
+
+box66.clickMe(); 
+
+
+function Person(name) {
+    this.name = name;
+}
+
+// ES5
+Person.prototype.myFriends5 = function(friends) {
+    var array = friends.map(function(element) {
+        return this.name + ' is friends with ' + element;
+    }.bind(this))
+
+    console.log(array);
+}
+
+var friends5 = ['Jane', 'Bob', 'Mark'];
+
+new Person('John').myFriends5(friends5);
+
+
+// ES6
+Person.prototype.myFriends6 = function(friends) {
+
+    const array = friends.map((element) => `${this.name} is friends with ${element}`)
+
+    console.log(array);
+}
+
+const friends6 = ['Jane', 'Bob', 'Mark'];
+
+new Person('Mike').myFriends6(friends6);

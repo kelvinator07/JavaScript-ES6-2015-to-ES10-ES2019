a349612c2f9a24b059ae4d93791456f5f88c2958
Kelvinator07 <kelvinator4leo@gmail.com>
2020-02-10 02:38:26 +0100

    Destructuring and Arrays in ES6

--- script.js
+++ script.js
@@ -204,3 +204,111 @@ Person.prototype.myFriends6 = function(friends) {
 const friends6 = ['Jane', 'Bob', 'Mark'];
 
 new Person('Mike').myFriends6(friends6);
+
+
+
+// Destructuring
+
+// ES5
+var kelvin = ['Kelvin', 25];
+var name = kelvin[0];
+var age = kelvin[1];
+
+// ES6
+// Array
+const [name6, age6] = ['Kelvin', 25];
+console.log(name6);
+console.log(age6);
+
+// Object
+const obj = {
+    firstName: 'Kelvin',
+    lastName: 'Geeky'
+}
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+const [age2, retirement] = calcAgeRetirement(1994);
+console.log(age2);
+console.log(retirement);
+
+
+// Arrays in ES6
+const boxes = document.querySelectorAll('.box');
+
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur) {
+    cur.style.backgroundColor = 'dodgerblue';
+});
+
+// ES6
+const boxesArr6 = Array.from(boxes);
+boxesArr6.forEach((cur) => cur.style.backgroundColor = 'dodgerblue');
+console.log(boxesArr5);
+
+// ES5 
+for (var i = 0; i < boxesArr5.length; i++) {
+    if (boxesArr5[i].className === 'box blue') {
+        continue;
+    }
+    boxesArr5[i].style.backgroundColor = 'dodgerblue';
+    boxesArr5[i].textContent = 'I changed to blue';
+}
+
+// ES6
+for (const each of boxesArr6) {
+    if (each.className.includes('blue')) {
+        continue;
+    }
+    each.style.backgroundColor = 'dodgerblue';
+    each.textContent = 'I changed to blue';
+}
+
+//ES5
+var ages = [11, 12, 27, 6];
+var fullAge = ages.map(function(cur) {
+    return cur >= 18;
+});
+console.log(fullAge);
+console.log(fullAge.indexOf(true));
+console.log(ages[fullAge.indexOf(true)]);
+
+
+//ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
